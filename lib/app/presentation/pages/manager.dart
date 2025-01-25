b63992b2fb5425c54e4f04d09f6d334281f591e6
xyloblonk <82177322+xyloblonk@users.noreply.github.com>
2025-01-25 12:56:27 -0800

    Create manager.dart

---
+++ lib/app/presentation/pages/manager.dart
@@ -0,0 +1,55 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'manager_model.dart';
+export 'manager_model.dart';
+
+class ManagerWidget extends StatefulWidget {
+  const ManagerWidget({super.key});
+
+  @override
+  State<ManagerWidget> createState() => _ManagerWidgetState();
+}
+
+class _ManagerWidgetState extends State<ManagerWidget> {
+  late ManagerModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => ManagerModel());
+
+    logFirebaseEvent('screen_view', parameters: {'screen_name': 'manager'});
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        body: SafeArea(
+          top: true,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
