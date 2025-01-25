abf40164277eba40b1534d7438127e16899fdbca
xyloblonk <82177322+xyloblonk@users.noreply.github.com>
2025-01-25 12:56:08 -0800

    Create billing.dart

---
+++ lib/app/presentation/pages/billing.dart
@@ -0,0 +1,138 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'billing_model.dart';
+export 'billing_model.dart';
+
+class BillingWidget extends StatefulWidget {
+  const BillingWidget({super.key});
+
+  @override
+  State<BillingWidget> createState() => _BillingWidgetState();
+}
+
+class _BillingWidgetState extends State<BillingWidget>
+    with TickerProviderStateMixin {
+  late BillingModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => BillingModel());
+
+    logFirebaseEvent('screen_view', parameters: {'screen_name': 'billing'});
+    _model.tabBarController = TabController(
+      vsync: this,
+      length: 3,
+      initialIndex: 0,
+    )..addListener(() => safeSetState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        body: SafeArea(
+          top: true,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment(0.0, 0),
+                      child: TabBar(
+                        labelColor: FlutterFlowTheme.of(context).primaryText,
+                        unselectedLabelColor:
+                            FlutterFlowTheme.of(context).secondaryText,
+                        labelStyle:
+                            FlutterFlowTheme.of(context).titleMedium.override(
+                                  fontFamily: 'Plus Jakarta Sans',
+                                  letterSpacing: 0.0,
+                                ),
+                        unselectedLabelStyle:
+                            FlutterFlowTheme.of(context).titleMedium.override(
+                                  fontFamily: 'Plus Jakarta Sans',
+                                  letterSpacing: 0.0,
+                                ),
+                        indicatorColor: FlutterFlowTheme.of(context).primary,
+                        tabs: [
+                          Tab(
+                            text: 'Billing',
+                          ),
+                          Tab(
+                            text: 'Manager',
+                          ),
+                          Tab(
+                            text: 'Settings',
+                          ),
+                        ],
+                        controller: _model.tabBarController,
+                        onTap: (i) async {
+                          [
+                            () async {
+                              logFirebaseEvent(
+                                  'BILLING_PAGE_Tab_vef32ax3_ON_TAP');
+                            },
+                            () async {
+                              logFirebaseEvent(
+                                  'BILLING_PAGE_Tab_caex1qy7_ON_TAP');
+
+                              context.pushNamed('manager');
+                            },
+                            () async {
+                              logFirebaseEvent(
+                                  'BILLING_PAGE_Tab_ir8zjti7_ON_TAP');
+
+                              context.pushNamed('settings');
+                            }
+                          ][i]();
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      child: TabBarView(
+                        controller: _model.tabBarController,
+                        children: [
+                          Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [],
+                          ),
+                          Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [],
+                          ),
+                          Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
